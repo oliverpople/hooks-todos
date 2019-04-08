def96fceaac3378fff9466c8b7dbc8d34f6fd9d3
oliverpople <oliverpople@gmail.com>
2019-04-08 16:43:15 -0700

    Removed previous App.js component and instead created an App component within the index.js fo the next example App.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-import React from "react";
+import React, { useContext, useReducer } from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-export const UserContext = React.createContext();
+const App = () => {};
 
-const username = "Dave";
-
-ReactDOM.render(
-  <UserContext.Provider value={username}>
-    <App />{" "}
-  </UserContext.Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
