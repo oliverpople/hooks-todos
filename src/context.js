bb0b2e9695d2b29490d013d46050220d140d5711
oliverpople <oliverpople@gmail.com>
2019-04-08 16:54:55 -0700

    Context file to createContext.

---
+++ src/context.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodosContext = React.createContext({
+  todos: [
+    { id: 1, text: "Eat Breakfast", complete: false },
+    { id: 2, text: "Do Laundry", complete: false },
+    { id: 3, text: "Finish Project", complete: true }
+  ]
+});
+
+export default TodosContext;
