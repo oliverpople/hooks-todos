674ee479183ab86915936546f5aa5acc9751597b
oliverpople <oliverpople@gmail.com>
2019-04-09 11:36:03 -0700

    Added a reducer case for adding new todo items via text form input.

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,14 @@
+import uuidv4 from "uuid/v4";
+
 export default function reducer(state, action) {
   switch (action.type) {
+    case "ADD_TODO":
+      const newToDo = { id: uuidv4(), text: action.payload, complete: false };
+      const addedTodos = [...state.todos, newToDo];
+      return {
+        ...state,
+        todos: addedTodos
+      };
     case "TOGGLE_TODO":
       const toggledTodos = state.todos.map(t =>
         t.id === action.payload.id
