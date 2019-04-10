eeb8697cd8723646a622760ca4646ecdb4f5c703
oliverpople <oliverpople@gmail.com>
2019-04-10 09:53:50 -0700

    App now persist toggle data.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -17,9 +17,13 @@ export default function TodoList() {
             className="flex items-center bg-orange-dark border-black border-dashed border-2 my-2 py-4"
           >
             <span
-              onDoubleClick={() =>
-                dispatch({ type: "TOGGLE_TODO", payload: todo })
-              }
+              onDoubleClick={async () => {
+                const response = await axios.patch(
+                  `https://hooks-api.oliverpople.now.sh/todos/${todo.id}`,
+                  { complete: !todo.complete }
+                );
+                dispatch({ type: "TOGGLE_TODO", payload: response.data });
+              }}
               className={`flex-1 ml-12 cursor-pointer ${todo.complete &&
                 "line-through text-grey-darkest"}`}
             >
